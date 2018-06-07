38373145d6e943dd24043d2788e619e37adfc6ef
Andreas Klapperich <Andreas@Klapperich.de>
2018-06-07 20:56:13 +0200

    ICHG: Own MvvmHelpers

--- TestApp/TestApp/MainPage.xaml.cs
+++ TestApp/TestApp/MainPage.xaml.cs
@@ -1,11 +1,9 @@
 ﻿using EifelMono.ScanCodeAnalyzer;
-using MvvmHelpers;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -15,7 +13,7 @@ namespace TestApp {
 	    public ScanCodeType Type { get; set; }
 	    public string Value { get; set; }
 	}
-	public class MvvmData : MvvmClass {
+	public class MvvmData : MvvmObject {
 
 	    private List<object> scanCodes = new List<object>() {
 		 new TVO() {Type= ScanCodeType.GS1, Value= @"0104150123456782101A234B5\x1D17151231211234567890123456" },
@@ -25,13 +23,12 @@ namespace TestApp {
 		 new TVO() {Type= ScanCodeType.GS1, Value= @"0104150123456782101A234B5\x1D17151231211234567890123456" },
 		 new TVO() {Type= ScanCodeType.IFA, Value= @"[)>\x1E06\x1D9N111234567842\x1D1T1A234B5\x1DD151231\x1DS1234567890123456\x1E\x04" }
 	    };
-
 	    public List<object> ScanCodes { get => scanCodes; set => scanCodes = value; }
 
 	    public ScanCodeValues ScanCodeValues { get; set; } = new ScanCodeValues();
 
-
 	    public MvvmProperty<string> SerialNumber { get; set; } = MvvmProperty.Create<string>();
+	    public MvvmProperty<DateTime> TimeStamp { get; set; } = MvvmProperty.Create<DateTime>();
 	}
 
 	public MvvmData Data { get; set; } = new MvvmData();
@@ -40,6 +37,12 @@ namespace TestApp {
 	{
 	    InitializeComponent();
 	    BindingContext = this;
+	    Task.Run(async () => {
+		while (true) {
+		    await Task.Delay(TimeSpan.FromSeconds(1));
+		    Data.TimeStamp.Value = DateTime.Now;
+		}
+	    });
 	}
 
 	private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
@@ -47,35 +50,10 @@ namespace TestApp {
 	    var o = ((sender as ListView).SelectedItem) as TVO;
 	    Data.ScanCodeValues = ScanCode.Analyze(o.Value);
 	    Data.SerialNumber.Value = Data.ScanCodeValues.SerialNumber;
-	    OnPropertyChanged("");
+	    // Data.Refresh();
 	}
     }
 
-    public class MvvmClass : INotifyPropertyChanged {
-	public event PropertyChangedEventHandler PropertyChanged;
-	protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = "")
-	{
-	    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-	}
-    }
 
-    public class MvvmProperty {
-	public static MvvmProperty<T> Create<T>([CallerMemberName]string propertyName = "")
-	{
-	    return new MvvmProperty<T>() { PropertyName = propertyName };
-	}
-    }
-    public class MvvmProperty<T> : INotifyPropertyChanged {
-
-	public T Value { get; set; }
-	public string PropertyName { get; set; }
-
-	public event PropertyChangedEventHandler PropertyChanged;
-
-	protected virtual void OnPropertyChanged(string propertyName = null)
-	{
-	    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName ?? PropertyName));
-	}
-    }
 }
 
---
+++ TestApp/TestApp/Mvvm.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TestApp {
+
+    public interface IOnPropertyChanged {
+	void OnPropertyChanged(string propertyName);
+    }
+
+    public interface IMvvmParent {
+	IOnPropertyChanged MvvmParent { get; set; }
+    }
+
+    public class MvvmObject : INotifyPropertyChanged, IOnPropertyChanged, IMvvmParent {
+
+	public MvvmObject()
+	{
+	    foreach (var property in MvvmProperties) {
+		property.MvvmParent = this;
+	    }
+	}
+
+	protected List<MvvmProperty> _MvvmProperties = null;
+	public List<MvvmProperty> MvvmProperties {
+	    get {
+		if (_MvvmProperties == null) {
+		    _MvvmProperties = new List<MvvmProperty>();
+		    var properties = this.GetType()
+			.GetProperties(BindingFlags.Instance | BindingFlags.Public)
+			.Where(ø => ø.PropertyType.IsSubclassOf(typeof(MvvmProperty)));
+		    foreach (var p in properties) {
+			var identifier = (MvvmProperty)(p.GetValue(this, null));
+			if (identifier != null)
+			    if (!_MvvmProperties.Contains(identifier))
+				_MvvmProperties.Add(identifier);
+		    }
+		}
+		return _MvvmProperties;
+	    }
+	}
+
+	public event PropertyChangedEventHandler PropertyChanged;
+
+	public IOnPropertyChanged MvvmParent { get; set; } = null;
+	public void OnPropertyChanged([CallerMemberName]string propertyName = "")
+	{
+	    (PropertyChanged)?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+	public void UseOnPropertyChanged(string propertyName = null)
+	{
+	    if (MvvmParent != null)
+		MvvmParent.OnPropertyChanged(propertyName);
+	    else
+		OnPropertyChanged(propertyName);
+	}
+
+	public void Refresh() => OnPropertyChanged(string.Empty);
+    }
+
+
+    public class MvvmProperty : INotifyPropertyChanged, IOnPropertyChanged, IMvvmParent {
+
+	public string PropertyName { get; set; }
+	public event PropertyChangedEventHandler PropertyChanged;
+	public IOnPropertyChanged MvvmParent { get; set; } = null;
+
+	public void OnPropertyChanged(string propertyName = null)
+	{
+	    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName ?? PropertyName));
+	}
+	public void UseOnPropertyChanged(string propertyName = null)
+	{
+	    if (MvvmParent != null)
+		MvvmParent.OnPropertyChanged(propertyName);
+	    else
+		OnPropertyChanged(propertyName);
+	}
+
+	public static MvvmProperty<T> Create<T>([CallerMemberName]string propertyName = "") where T : IComparable
+	{
+	    return new MvvmProperty<T>() { PropertyName = propertyName };
+	}
+
+    }
+    public class MvvmProperty<T> : MvvmProperty where T : IComparable {
+
+	protected T _Value;
+	public T Value {
+	    get => _Value; set {
+		try {
+		    if (value.CompareTo(_Value) != 0) {
+			_Value = value;
+			UseOnPropertyChanged();
+		    }
+		} catch (Exception ex) {
+		    Debug.WriteLine(ex);
+		    _Value = value;
+		    UseOnPropertyChanged();
+		}
+
+	    }
+	}
+    }
+}
